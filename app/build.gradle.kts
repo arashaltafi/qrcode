6db1da5ca026eab7bde55a9d0e4f4aa29fe1c1d6
Arash Altafi <arashaltafi1377@gmail.com>
2025-12-06 17:30:03 +0330

    aaa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,12 +112,9 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
 
     // Material
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material)
     implementation(libs.androidx.material.icons.extended.android)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
